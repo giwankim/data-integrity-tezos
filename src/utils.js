e51c8bfd333377817e9adc06d6f210ad47ad7d6b
Giwan Kim <giwan.kim@gmail.com>
2021-05-05 05:48:52 +0000

    Add listDir to utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
 
 async function openLog(logPath) {
     try {
@@ -18,15 +20,30 @@ async function addLog(logPath, filePath, integrity) {
     }
 }
 
-// const path = require('path');
+const isFile = fileName => {
+    return fs.lstatSync(fileName).isFile();
+}
+
+async function listDir(folderPath) {
+    return fs.readdirSync(folderPath).map(fileName => {
+        return path.join(folderPath, fileName);
+    })
+    .filter(isFile);
+}
+
 // const logPath = path.join(__dirname, 'log.log');
 // (async function () {
 //     await openLog(logPath);
 //     await addLog(logPath, logPath, true);
 //     await addLog(logPath, logPath, false);
 // })();
+// (async function() {
+//     files = await listDir('.');
+//     console.log(files);
+// })();
 
 module.exports = {
     openLog: openLog,
     addLog: addLog,
+    listDir: listDir,
 };
