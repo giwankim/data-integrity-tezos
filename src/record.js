05327a4774654b0e99750cc5a6f61d7e8841ea22
Giwan Kim <giwan.kim@gmail.com>
2021-05-07 06:49:25 +0000

    Clean up record.js

--- src/record.js
+++ src/record.js
@@ -1,32 +1,20 @@
 #!/usr/bin/env node
 require('dotenv').config()
-
 const path = require('path');
-const { TezosToolkit, MichelsonMap } = require('@taquito/taquito');
-const { InMemorySigner } = require('@taquito/signer');
-
 const utils = require('./utils');
+const kogi = require('./kogi');
 
-const contractAddress = 'KT1GmLE7QQ9NGAR1wbaD9nEJYqb562bZYqW6'
-
-async function recordFile(filePath, algorithm = 'sha256') {
+async function recordFile(contractAddress, filePath, algorithm = 'sha256') {
     // Generate package id and content hash
-    let pkgId, hashSum;
-    try {
-        pkgId = utils.generateId(filePath, algorithm);
-        hashSum = utils.checksum(filePath, algorithm);
-    } catch (error) {
-        console.error(`Error generating package id or content hash: ${error.message}`);
-        process.exit(1);
-    }
+    const pkgId = utils.generateId(filePath, algorithm);
+    const hashSum = utils.checksum(filePath, algorithm);
 
-    const Tezos = new TezosToolkit(process.env.RPC);
-    Tezos.setProvider({ signer: new InMemorySigner(process.env.KEY), })
-
+    // Add id -> hash to contract storage map
+    const Tezos = await kogi.signerFactory(process.env.RPC, process.env.KEY);
     Tezos.contract
         .at(contractAddress)
         .then((contract) => {
-            console.log(`Recording...`);
+            console.log(`Recording ${pkgId} -> ${hashSum} to ${contractAddress}`);
             return contract.methods.record(pkgId, hashSum).send();
         })
         .then((op) => {
@@ -43,7 +31,10 @@ async function recordFile(filePath, algorithm = 'sha256') {
 }
 
 (async() => {
+    const contractAddress = 'KT1GmLE7QQ9NGAR1wbaD9nEJYqb562bZYqW6';
     const fileName = './utils.js';
     const filePath = path.join(__dirname, fileName);
-    await recordFile(filePath);
+    await recordFile(contractAddress, filePath);
 })();
+
+exports.recordFile = recordFile;
