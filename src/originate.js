289d42b4add8a8f7cddf5ba1615d000088c89c7e
Giwan Kim <giwan.kim@gmail.com>
2021-05-10 07:33:49 +0000

    Streamline

--- src/originate.js
+++ src/originate.js
@@ -1,16 +1,12 @@
-#!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
 const { MichelsonMap } = require('@taquito/taquito');
 const kogi = require('./kogi');
 
-async function originate(contractPath) {
-    console.log(`Originating of contract at ${contractPath}...`);
+async function originate(Tezos, contractPath) {
+    console.log(`Originating contract at ${contractPath}...`);
     try {
         const codeJSONFile = fs.readFileSync(contractPath, 'utf8');
-
-        const Tezos = await kogi.faucetSignerFactory();
-
         const storage = new MichelsonMap();
         const originationOp = await Tezos.contract.originate({
             code: codeJSONFile,
@@ -23,11 +19,5 @@ async function originate(contractPath) {
         console.error(`Error: ${error.message}`);
     }
 }
-
-(async() => {
-    const contractFile = '../build/EvenSimpler.michelson';
-    const contractPath = path.join(__dirname, contractFile);
-    await originate(contractPath);
-})();
 
 exports.originate = originate;
--- src/record.js
+++ src/record.js
@@ -1,10 +1,9 @@
-#!/usr/bin/env node
 require('dotenv').config();
 const path = require('path');
 const utils = require('./utils');
 const kogi = require('./kogi');
 
-async function recordFile(contractAddress, filePath, algorithm = 'sha256') {
+async function record(Tezos, contractAddress, filePath, algorithm = 'sha256') {
     // Get package id and checksum
     const pkgId = utils.generateId(filePath, algorithm);
     const hashSum = utils.checksum(filePath, algorithm);
@@ -12,9 +11,8 @@ async function recordFile(contractAddress, filePath, algorithm = 'sha256') {
     // Add id -> hash to storage
     console.log(`Recording ${pkgId} -> ${hashSum} to ${contractAddress}`);
     try {
-        const Tezos = await kogi.signerFactory();
         const contract = await Tezos.contract.at(contractAddress);
-        const op = await contract.methods.record(pkgId, hashSum).send();
+        const op = await contract.methods.record(hashSum, pkgId).send();
 
         const confirmation = process.env.CONFIRMATION;
         console.log(`Awaiting ${confirmation} blocks for ${op.hash} to be confirmed...`);
@@ -26,11 +24,4 @@ async function recordFile(contractAddress, filePath, algorithm = 'sha256') {
     }
 }
 
-(async() => {
-    const contractAddress = 'KT19sa3bBSXjQ83miwjSQMZjKQ7u9HUT4Xzt';
-    const fileName = './utils.js';
-    const filePath = path.join(__dirname, fileName);
-    await recordFile(contractAddress, filePath);
-})();
-
-exports.recordFile = recordFile;
+exports.record = record;
