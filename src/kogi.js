b4762be7f1b8122348d2ff463bbbc18ac4ca9eea
Giwan Kim <giwan.kim@gmail.com>
2021-05-10 02:07:58 +0000

    Cleanup args to factory methods

--- src/kogi.js
+++ src/kogi.js
@@ -1,11 +1,17 @@
+require('dotenv').config();
 const fs = require('fs');
 const { TezosToolkit } = require('@taquito/taquito');
 const { InMemorySigner, importKey } = require('@taquito/signer');
 
-async function signerFactory(rpcUrl, secretKey) {
-    const Tezos = new TezosToolkit(rpcUrl);
-    await Tezos.setProvider({ signer: await InMemorySigner.fromSecretKey(secretKey) });
-    return Tezos;
+async function signerFactory() {
+    try {
+        const Tezos = new TezosToolkit(process.env.RPC);
+        const signer = await InMemorySigner.fromSecretKey(process.env.KEY);
+        await Tezos.setProvider({ signer: signer });
+        return Tezos;
+    } catch (error) {
+        console.error(`Error importing secret key: ${error.message}`);
+    }
 }
 
 async function getFaucetKey(faucetPath) {
@@ -13,13 +19,13 @@ async function getFaucetKey(faucetPath) {
         const faucetData = await fs.promises.readFile(faucetPath);
         return JSON.parse(faucetData);
     } catch (error) {
-        console.error(`Error trying to read faucet key: ${error.message}`);
+        console.error(`Error reading faucet key file: ${error.message}`);
     }
 }
 
-async function faucetSignerFactory(rpcUrl, faucetPath) {
-    const Tezos = new TezosToolkit(rpcUrl);
-    const { email, password, mnemonic, secret } = await getFaucetKey(faucetPath);
+async function faucetSignerFactory() {
+    const Tezos = new TezosToolkit(process.env.RPC);
+    const { email, password, mnemonic, secret } = await getFaucetKey(process.env.FAUCET_KEY);
     await importKey(
         Tezos,
         email,
